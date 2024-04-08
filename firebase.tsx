aa2b60db75597181665d12f8ac72687e104db88d
Tayala2272 <szyszczka@gmail.com>
2024-04-08 10:06:46 +0200

    firebase

--- firebase.tsx
+++ firebase.tsx
@@ -1,16 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
 
-const firebaseConfig = {
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+
+firebase.initializeApp({
   apiKey: "AIzaSyDq6AGj-JQy2aD7_0QKHNGlgudcUYlf-x8",
   authDomain: "inf00-7db24.firebaseapp.com",
   databaseURL: "https://inf00-7db24-default-rtdb.europe-west1.firebasedatabase.app",
@@ -19,13 +19,11 @@ const firebaseConfig = {
   messagingSenderId: "269971934067",
   appId: "1:269971934067:web:5ac50fc4d42539bfe0d306",
   measurementId: "G-L9GNY10JLF",
-};
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
 
-export default db
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
 
